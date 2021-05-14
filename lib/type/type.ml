54b15778217e8bf929bbefb41156fa3b1bd26981
Isaac-DeFrain <isaacdefrain@gmail.com>
2021-05-14 12:22:33 -0600

    Big_int: finish type implementation
      Add more exposed methods

--- lib/type/type.ml
+++ lib/type/type.ml
@@ -3,6 +3,34 @@ type t = int list
 
 let t_of_int i = [i]
 
+let t_of_list l = l
+
+let zero = [0]
+
+let one = [1]
+
+let compare a b =
+  let open List in
+  let len_a = length a in
+  let len_b = length b in
+  if len_a < len_b then -1
+  else
+    if len_a > len_b then 1
+    else
+      if len_a = 0 then 0
+      else
+        let rec cmp_helper len rev_a rev_b =
+          if len = 0 then 0
+          else
+            let msc_a = hd rev_a in
+            let msc_b = hd rev_b in
+            if msc_a < msc_b then -1
+            else
+              if msc_a > msc_b then 1
+              else cmp_helper (len - 1) (tl rev_a) (tl rev_b)
+        in
+        cmp_helper len_a (rev a) (rev b)
+
 let rec add_one = function
 | [] -> [1]
 | h :: tl ->
@@ -43,12 +71,24 @@ let rec sub x y = match (x, y) with
 let mul a b =
   let rec mul_helper acc a = function
   | [] -> acc
-  | [0] -> [0]
-  | 1 :: tl -> mul_helper (add acc a) a tl
+  | 0 :: tl -> mul_helper acc a tl
   | h :: tl -> mul_helper (add acc a) a (h - 1 :: tl)
   in
   mul_helper [0] a b
 
-let div _a _b = []
+let div num denom =
+  let rec div_helper q n d =
+    let cmp = compare n d in
+    if cmp < 0 then q
+    else
+      if cmp = 0 then add q one
+      else div_helper (add q one) (sub n d) d
+  in
+  div_helper zero num denom
 
-let mod_big _a _b = []
+let rec mod_big n m =
+  let cmp = compare n m in
+  if cmp < 0 then n
+  else
+    if cmp = 0 then zero
+    else mod_big (sub n m) m
--- lib/type/type.mli
+++ lib/type/type.mli
@@ -2,6 +2,14 @@ type t
 
 val t_of_int : int -> t
 
+val t_of_list : int list -> t
+
+val zero : t
+
+val one : t
+
+val compare : t -> t -> int
+
 val add : t -> t -> t
 
 val sub : t -> t -> t
